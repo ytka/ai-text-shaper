5aa0e55ad716ed322a8fa2fcf0ef970addcb7f00
ytka <yutakayy@gmail.com>
2024-06-23 23:39:09 +0900

    Update Hello World message, correct error message format, and modify TODO items.

--- internal/steps/write.go
+++ internal/steps/write.go
@@ -8,7 +8,7 @@ import (
 // WriteResult writes the outputText to the given outpath. If an error occurs, it wraps it with additional context.
 func WriteResult(outputText, outpath string) error {
 	if err := os.WriteFile(outpath, []byte(outputText), 0o600); err != nil {
-		return fmt.Errorf("error writing to file: %w", err) // fixed: Error messages should start with lowercase letter and should not include punctuation. Refer to the section "Error Strings" in the Google Go style guide.
+		return fmt.Errorf("error writing to file: %w", err)
 	}
 	return nil
 }
